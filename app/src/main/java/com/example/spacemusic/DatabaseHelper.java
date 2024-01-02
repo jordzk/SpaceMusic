466ec3d490f46521abc921f954688d4594bae7f3
jordzk <149849603+jordzk@users.noreply.github.com>
2024-01-02 18:46:01 -0500

    plantilla de basededatos

---
+++ app/src/main/java/com/example/spacemusic/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.example.spacemusic;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "reproductordb";
+    private static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase db;
+
+    // Constructor
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
